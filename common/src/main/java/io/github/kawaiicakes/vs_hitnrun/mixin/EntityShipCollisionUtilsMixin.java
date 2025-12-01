7209040070997a85f89fd02d1a6f84afebc5053c
kawaiicakes <vomitmopper@gmail.com>
2025-11-30 20:37:47 -0700

    further bug fixes. this might be ready for release soon

--- common/src/main/java/io/github/kawaiicakes/vs_hitnrun/mixin/EntityShipCollisionUtilsMixin.java
+++ common/src/main/java/io/github/kawaiicakes/vs_hitnrun/mixin/EntityShipCollisionUtilsMixin.java
@@ -69,10 +69,9 @@ public abstract class EntityShipCollisionUtilsMixin {
                     ship.getVelocity(), ship.getOmega()
             );
 
-            // TODO - this should consider the entity's absolute velocity; assuming default config settings, if the
-            //  entity's velocity is contributing 60% of a collision, but the collision velocity is something like
-            //  30000 m/s, obviously damage should still be applied
-            if (movement.scale(VSHitNRunConfig.SERVER.getEntityVelocityThreshold()).lengthSqr() >= properDeltaV.lengthSqr())
+            double entityVelocityThreshold = movement.lengthSqr() * VSHitNRunConfig.SERVER.getEntityVelocityThreshold();
+            double remainingVelocity = properDeltaV.lengthSqr() * (1 - VSHitNRunConfig.SERVER.getEntityVelocityThreshold());
+            if (entityVelocityThreshold >= properDeltaV.lengthSqr() && remainingVelocity < VSHitNRunConfig.SERVER.getSpeedThreshold())
                 return;
 
             ((Roadkillable) (Object) entity).vs_hitnrun$onRoadkill(
